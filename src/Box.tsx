2fcf72b89d7acd629338e7b05ebf9a98ffa2bb2b
James Barber <jamesbarber@twinsbaseball.com>
2023-06-10 09:35:46 -0400

    Add useRef, useEffect, and useFrame functionality

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useRef, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 interface Props {
   position: number[];
@@ -8,8 +10,23 @@ interface Props {
 }
 
 export default function Box({ position, name, color, wireframe }: Props) {
+  const ref = useRef();
+
+  useEffect(() => {
+    console.log(ref.current);
+  });
+
+  useFrame((_, delta) => {
+    // @ts-ignore
+    ref.current.rotation.x += 1 * delta;
+    // @ts-ignore
+    ref.current.rotation.y += 0.5 * delta;
+    // @ts-ignore
+    // ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 2) / 2;
+  });
+
   return (
-    <mesh position={position} name={name}>
+    <mesh position={position} name={name} ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color={color} wireframe={wireframe} />
     </mesh>
