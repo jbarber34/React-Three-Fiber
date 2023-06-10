07276e62c1100e7fe79696e1445d837e05ddf4f3
James Barber <jamesbarber@twinsbaseball.com>
2023-06-10 16:44:14 -0400

    Implement useState and useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Canvas } from '@react-three/fiber';
 import Box from './Box';
 import * as React from 'react';
+import * as THREE from 'three';
 
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
       <Box
-        position={[-0.75, 0, 0]}
+        position={new THREE.Vector3(-0.75, 0, 0)}
         name='A'
-        color={0x00ff00}
+        primaryColor={0x00ff00}
+        secondaryColor={0xff0000}
         wireframe={true}
       />
       <Box
-        position={[0.75, 0, 0]}
+        position={new THREE.Vector3(0.75, 0, 0)}
         name='B'
-        color={0xff0000}
+        primaryColor={0xff0000}
+        secondaryColor={0x00ff00}
         wireframe={false}
       />
     </Canvas>
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,28 +1,40 @@
 import * as React from 'react';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
 
 interface Props {
-  position: number[];
+  position: THREE.Vector3;
   name: string;
-  color: {};
+  primaryColor: {};
+  secondaryColor: {};
   wireframe?: boolean;
 }
 
-export default function Box({ position, name, color, wireframe }: Props) {
+export default function Box({
+  position,
+  name,
+  primaryColor,
+  secondaryColor,
+  wireframe,
+}: Props) {
   const ref = useRef();
+  const [count, setCount] = useState(0);
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.785398)],
+    []
+  );
 
   useEffect(() => {
-    console.log(ref.current);
+    // @ts-ignore
+    console.log(ref.current.geometry.uuid);
   });
 
   useFrame((_, delta) => {
     // @ts-ignore
-    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.x += delta;
     // @ts-ignore
     ref.current.rotation.y += 0.5 * delta;
-    // @ts-ignore
-    // ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 2) / 2;
   });
 
   return (
@@ -30,14 +42,53 @@ export default function Box({ position, name, color, wireframe }: Props) {
       position={position}
       name={name}
       ref={ref}
-      onPointerDown={(e) => console.log('pointer down', e.eventObject.name)}
-      onPointerUp={(e) => console.log('pointer up', e.eventObject.name)}
-      onPointerOver={(e) => console.log('pointer over', e.eventObject.name)}
-      onPointerOut={(e) => console.log('pointer out', e.eventObject.name)}
-      onUpdate={(e: any) => console.log(e)}
+      onPointerDown={() => setCount((count + 1) % 2)}
+      geometry={geometry[count]}
     >
-      <boxGeometry />
-      <meshBasicMaterial color={color} wireframe={wireframe} />
+      <meshBasicMaterial color={'lime'} wireframe />
     </mesh>
   );
+
+  // BEFORE useMemo
+  // const ref = useRef();
+  // const [hovered, setHover] = useState(false);
+  // const [rotate, setRotate] = useState(false);
+
+  // useEffect(() => {
+  //   console.log('Effect Fired');
+  // });
+
+  // useFrame((_, delta) => {
+  //   if (rotate) {
+  //     // @ts-ignore
+  //     ref.current.rotation.x += 1 * delta;
+  //     // @ts-ignore
+  //     ref.current.rotation.y += 0.5 * delta;
+  //     // @ts-ignore
+  //     // ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 2) / 2;
+  //   }
+  // });
+
+  // return (
+  //   <mesh
+  //     position={position}
+  //     name={name}
+  //     ref={ref}
+  //     // onPointerDown={(e) => console.log('pointer down', e.eventObject.name)}
+  //     // onPointerUp={(e) => console.log('pointer up', e.eventObject.name)}
+  //     // onPointerOver={(e) => console.log('pointer over', e.eventObject.name)}
+  //     // onPointerOut={(e) => console.log('pointer out', e.eventObject.name)}
+  //     // onUpdate={(e: any) => console.log(e)}
+  //     scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+  //     onPointerDown={() => setRotate(!rotate)}
+  //     onPointerOver={() => setHover(true)}
+  //     onPointerOut={() => setHover(false)}
+  //   >
+  //     <boxGeometry />
+  //     <meshBasicMaterial
+  //       color={hovered ? secondaryColor : primaryColor}
+  //       wireframe={wireframe}
+  //     />
+  //   </mesh>
+  // );
 }
