cccca9cf8c298525058ad584cf866dcba9221797
James Barber <jamesbarber@twinsbaseball.com>
2023-06-10 21:43:02 -0400

    Implement Sharing Objects and create new Polyhedron component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Canvas } from '@react-three/fiber';
-import Box from './Box';
+// import Box from './Box';
 import * as React from 'react';
 import * as THREE from 'three';
+import Polyhedron from './Polyhedron';
 
 export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ];
+
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box
+    <Canvas camera={{ position: [0, 0, 4] }}>
+      {/* <Box
         position={new THREE.Vector3(-0.75, 0, 0)}
         name='A'
         primaryColor={0x00ff00}
@@ -19,6 +26,22 @@ export default function App() {
         primaryColor={0xff0000}
         secondaryColor={0x00ff00}
         wireframe={false}
+      /> */}
+      <Polyhedron
+        position={new THREE.Vector3(-0.75, -0.75, 0)}
+        polyhedron={polyhedron}
+      />
+      <Polyhedron
+        position={new THREE.Vector3(0.75, -0.75, 0)}
+        polyhedron={polyhedron}
+      />
+      <Polyhedron
+        position={new THREE.Vector3(-0.75, 0.75, 0)}
+        polyhedron={polyhedron}
+      />
+      <Polyhedron
+        position={new THREE.Vector3(0.75, 0.75, 0)}
+        polyhedron={polyhedron}
       />
     </Canvas>
   );
---
+++ src/Polyhedron.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useRef, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+interface Props {
+  position: THREE.Vector3;
+  polyhedron: any;
+}
+
+export default function Polyhedron({ position, polyhedron }: Props) {
+  const ref = useRef();
+  const [count, setCount] = useState(0);
+
+  console.log(polyhedron);
+
+  useFrame((_, delta) => {
+    // @ts-ignore
+    ref.current.rotation.x += delta;
+    // @ts-ignore
+    ref.current.rotation.y += 0.5 * delta;
+  });
+
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerDown={() => {
+        setCount((count + 1) % 3);
+      }}
+      geometry={polyhedron[count]}
+    >
+      <meshBasicMaterial color={'lime'} wireframe />
+    </mesh>
+  );
+}
