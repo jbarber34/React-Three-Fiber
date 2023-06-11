4dc3b56971fdda60ca867f3ac0e1877a9841fa06
James Barber <jamesbarber@twinsbaseball.com>
2023-06-11 16:47:34 -0400

    Implement Leva GUI

--- src/App.tsx
+++ src/App.tsx
@@ -6,67 +6,115 @@ import Polyhedron from './Polyhedron';
 // import { Stats } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
+import { useMemo } from 'react';
 
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398),
-  ];
+  const polyhedron = useMemo(
+    () => [
+      new THREE.BoxGeometry(),
+      new THREE.SphereGeometry(0.785398),
+      new THREE.DodecahedronGeometry(0.785398),
+    ],
+    []
+  );
+
+  const options = useMemo(() => {
+    return {
+      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+      visible: true,
+      color: { value: 'lime' },
+    };
+  }, []);
+
+  const pA = useControls('Polyhedron A', options);
+  const pB = useControls('Polyhedron B', options);
 
   return (
-    <Canvas camera={{ position: [0, 0, 4] }}>
-      {/* <Box
-        position={new THREE.Vector3(-0.75, 0, 0)}
-        name='A'
-        primaryColor={0x00ff00}
-        secondaryColor={0xff0000}
-        wireframe={true}
-      />
-      <Box
-        position={new THREE.Vector3(0.75, 0, 0)}
-        name='B'
-        primaryColor={0xff0000}
-        secondaryColor={0x00ff00}
-        wireframe={false}
-      /> */}
+    <Canvas camera={{ position: [1, 2, 4] }}>
       <Polyhedron
-        position={new THREE.Vector3(-0.75, -0.75, 0)}
+        position={[-1, 1, 0]}
+        rotation={[pA.x, pA.y, pA.z]}
+        visible={pA.visible}
+        color={pA.color}
         polyhedron={polyhedron}
       />
       <Polyhedron
-        position={new THREE.Vector3(0.75, -0.75, 0)}
+        position={[1, 1, 0]}
+        rotation={[pB.x, pB.y, pB.z]}
+        visible={pB.visible}
+        color={pB.color}
         polyhedron={polyhedron}
       />
-      <Polyhedron
-        position={new THREE.Vector3(-0.75, 0.75, 0)}
-        polyhedron={polyhedron}
-      />
-      <Polyhedron
-        position={new THREE.Vector3(0.75, 0.75, 0)}
-        polyhedron={polyhedron}
-      />
-      <OrbitControls
-      // enableDamping={false}
-      // enablePan={false}
-      // enableRotate={false}
-      // Limit the amount of rotation
-      // minAzimuthAngle={-Math.PI / 4}
-      // maxAzimuthAngle={Math.PI / 4}
-      // minPolarAngle={Math.PI / 6}
-      // maxPolarAngle={Math.PI - Math.PI / 6}
-      />
+      <OrbitControls target-y={1} />
       <axesHelper args={[5]} />
-      <gridHelper
-        args={[20, 20, 0xff0000, 'teal']}
-        // rotation-x={Math.PI / 4} //  Target a specific axis
-        rotation={[Math.PI / 4, 0, 0]} // Do X, Y, and Z all at once.
-      />
-
-      {/* Show Megabytes Used as default */}
-      {/* <Stats showPanel={2} /> */}
-      {/* More advance monitoring than Stats */}
+      <gridHelper />
       <Perf position='top-left' />
     </Canvas>
   );
+
+  // BEFORE Leva
+  // const polyhedron = [
+  //   new THREE.BoxGeometry(),
+  //   new THREE.SphereGeometry(0.785398),
+  //   new THREE.DodecahedronGeometry(0.785398),
+  // ];
+
+  // return (
+  //   <Canvas camera={{ position: [0, 0, 4] }}>
+  //     {/* <Box
+  //       position={new THREE.Vector3(-0.75, 0, 0)}
+  //       name='A'
+  //       primaryColor={0x00ff00}
+  //       secondaryColor={0xff0000}
+  //       wireframe={true}
+  //     />
+  //     <Box
+  //       position={new THREE.Vector3(0.75, 0, 0)}
+  //       name='B'
+  //       primaryColor={0xff0000}
+  //       secondaryColor={0x00ff00}
+  //       wireframe={false}
+  //     /> */}
+  //     <Polyhedron
+  //       position={new THREE.Vector3(-0.75, -0.75, 0)}
+  //       polyhedron={polyhedron}
+  //     />
+  //     <Polyhedron
+  //       position={new THREE.Vector3(0.75, -0.75, 0)}
+  //       polyhedron={polyhedron}
+  //     />
+  //     <Polyhedron
+  //       position={new THREE.Vector3(-0.75, 0.75, 0)}
+  //       polyhedron={polyhedron}
+  //     />
+  //     <Polyhedron
+  //       position={new THREE.Vector3(0.75, 0.75, 0)}
+  //       polyhedron={polyhedron}
+  //     />
+  //     <OrbitControls
+  //     // enableDamping={false}
+  //     // enablePan={false}
+  //     // enableRotate={false}
+  //     // Limit the amount of rotation
+  //     // minAzimuthAngle={-Math.PI / 4}
+  //     // maxAzimuthAngle={Math.PI / 4}
+  //     // minPolarAngle={Math.PI / 6}
+  //     // maxPolarAngle={Math.PI - Math.PI / 6}
+  //     />
+  //     <axesHelper args={[5]} />
+  //     <gridHelper
+  //       args={[20, 20, 0xff0000, 'teal']}
+  //       // rotation-x={Math.PI / 4} //  Target a specific axis
+  //       rotation={[Math.PI / 4, 0, 0]} // Do X, Y, and Z all at once.
+  //     />
+
+  //     {/* Show Megabytes Used as default */}
+  //     {/* <Stats showPanel={2} /> */}
+  //     {/* More advance monitoring than Stats */}
+  //     <Perf position='top-left' />
+  //   </Canvas>
+  // );
 }
--- src/Polyhedron.tsx
+++ src/Polyhedron.tsx
@@ -1,37 +1,64 @@
 import * as React from 'react';
 import { useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
+// import { useFrame } from '@react-three/fiber';
 
 interface Props {
-  position: THREE.Vector3;
+  position: any;
+  rotation: any;
+  visible: any;
   polyhedron: any;
+  color: any;
 }
 
-export default function Polyhedron({ position, polyhedron }: Props) {
+export default function Polyhedron({
+  position,
+  rotation,
+  visible,
+  polyhedron,
+  color,
+}: Props) {
   const ref = useRef();
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(2);
 
-  console.log(polyhedron);
-
-  useFrame((_, delta) => {
-    // @ts-ignore
-    ref.current.rotation.x += delta;
-    // @ts-ignore
-    ref.current.rotation.y += 0.5 * delta;
-  });
+  console.log(polyhedron[count].uuid);
 
   return (
     <mesh
       position={position}
+      rotation={rotation}
+      visible={visible}
       ref={ref}
       onPointerDown={() => {
         setCount((count + 1) % 3);
       }}
       geometry={polyhedron[count]}
     >
-      <meshBasicMaterial color={'lime'} wireframe />
-      {/* Can add axes helper to each mesh if you want */}
-      {/* <axesHelper /> */}
+      <meshBasicMaterial color={color} wireframe />
     </mesh>
   );
+
+  //  BEFORE Leva
+  // const ref = useRef();
+  // const [count, setCount] = useState(0);
+  // console.log(polyhedron);
+  // useFrame((_, delta) => {
+  //   // @ts-ignore
+  //   ref.current.rotation.x += delta;
+  //   // @ts-ignore
+  //   ref.current.rotation.y += 0.5 * delta;
+  // });
+  // return (
+  //   <mesh
+  //     position={position}
+  //     ref={ref}
+  //     onPointerDown={() => {
+  //       setCount((count + 1) % 3);
+  //     }}
+  //     geometry={polyhedron[count]}
+  //   >
+  //     <meshBasicMaterial color={'lime'} wireframe />
+  //     {/* Can add axes helper to each mesh if you want */}
+  //     {/* <axesHelper /> */}
+  //   </mesh>
+  // );
 }
